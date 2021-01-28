a436b064c0e92936a57da0deb322554deb9dcb77
Dillon <dillonlara115@gmail.com>
2021-01-28 09:16:04 -0700

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+    <div class="w-100">
+      <h1 class="mb-0">
+        Dillon
+        <span class="text-primary">Lara</span>
+      </h1>
+      <div class="subheading mb-5">
+        Colorado Springs, CO 80920 · (573) 256-9991 ·
+        <a
+          href="mailto:dillonlara115@gmail.com"
+        >dillonlara115@gmail.com</a>
+      </div>
+      <p
+        class="lead mb-5"
+      >      I'm a <strong>Web Developer</strong> currently working as a freelance developer specializing in <a href="#test">UX design best practices</a> for ecommerce sites and a technical SEO developer helping clients improve their search engine rankings.  Previously I was a Web Development Manager for a Creative Agency.  When I'm not helping clients grow their online presence, I can be found building out my own apps or mountain biking with my family.</p>
+      <p class="lead mb-5">
+        Get in touch with me here:
+        <a
+      </p>
+      <div class="social-icons">
+        <a href="https://www.linkedin.com/in/dillon-lara/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
+          <font-awesome :icon="['fab', 'linkedin']"/>
+        </a>
+        <a href="https://github.com/dillonlara115" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile">
+          <font-awesome :icon="['fab', 'github']"/>
+        </a>
+        <a href="https://www.instagram.com/dillonlara115/" target="_blank" rel="noopener noreferrer" aria-label="Instagram Profile">
+          <font-awesome :icon="['fab', 'instagram']"/>
+        </a>
+        <a href="https://loke.dev" target="_blank" rel="noopener noreferrer" aria-label="Loke.dev">
+          <font-awesome :icon="['fas', 'rss']"/>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.social-icons a {
+  cursor: pointer;
+  display: inline-block;
+  height: 3.5rem;
+  width: 3.5rem;
+  background-color: var(--secondary);
+  color: #fff !important;
+  border-radius: 100%;
+  text-align: center;
+  font-size: 1.5rem;
+  line-height: 3.5rem;
+  margin-right: 1rem;
+
+  @media (max-width: 380px) {
+    margin-right: 0.5rem;
+  }
+
+  @media (max-width: 350px) {
+    margin-right: 0.2rem;
+  }
+}
+
+.social-icons a:last-child {
+  margin-right: 0;
+}
+
+.social-icons a:hover {
+  background-color: var(--primary);
+}
+</style>
